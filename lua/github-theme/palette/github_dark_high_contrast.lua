9fe90b65243bad47284993ee17bb4333c83b7e2e
ryangco <135596330+ryangco@users.noreply.github>
2024-05-29 19:22:00 +0800

    dark high contrast

--- lua/github-theme/palette/github_dark_high_contrast.lua
+++ lua/github-theme/palette/github_dark_high_contrast.lua
@@ -125,7 +125,7 @@ local function generate_spec(pal)
   -- stylua: ignore start
   local spec = {
     bg0  = alpha(C(pal.canvas.inset), 0.75),            -- Dark bg (popup and float)
-    bg1  = pal.canvas.default,                          -- Default bg
+    bg1  = "#000000",
     bg2  = alpha(C(pal.neutral.emphasis), 0.1),         -- Lighter bg (colorcolumn Folds)
     bg3  = alpha(C(pal.fg.default), 0.1),               -- Lighter bg (cursor line)
     bg4  = pal.scale.gray[4],                           -- Conceal
